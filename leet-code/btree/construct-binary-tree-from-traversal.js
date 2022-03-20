2bd9141ff7b88bb06f0700dd5e3c2e4eb5098108
Jack Li <dreamer22qq@gmail.com>
2022-03-20 12:20:27 +0800

    from inorder postorder

--- leet-code/btree/construct-binary-tree-from-traversal.js
+++ leet-code/btree/construct-binary-tree-from-traversal.js
@@ -1,4 +1,4 @@
-import { TreeNode, visitPostorder } from "./tree-node.js";
+import { TreeNode, visitPostorder, visitPreorder } from "./tree-node.js";
 import { equals } from "../base/equals.js";
 
 /**
@@ -66,7 +66,76 @@ function buildFromPreInorder(
   return node;
 }
 
+/**
+ * 从中序后后序构建二叉树
+ * @param {Array} midorder
+ * @param {Array} postorder
+ */
+function buildFromInPostOrder(inorder, postorder) {
+  return buildFromInPost(
+    inorder,
+    0,
+    inorder.length - 1,
+    postorder,
+    0,
+    postorder.length - 1
+  );
+}
+
+/**
+ *
+ * @param {Array} inorder
+ * @param {number} inleft
+ * @param {number} inright
+ * @param {Array} postorder
+ * @param {number*} postleft
+ * @param {number*} postright
+ */
+function buildFromInPost(
+  inorder,
+  inleft,
+  inright,
+  postorder,
+  postleft,
+  postright
+) {
+  if (inleft > inright || postleft > postright) {
+    return null;
+  }
+  let root = postorder[postright];
+  let node = new TreeNode(root);
+  if (inleft === inright) {
+    return node;
+  }
+  let index = inorder.lastIndexOf(root, inright);
+  let leftSize = index - inleft;
+
+  node.left = buildFromInPost(
+    inorder,
+    inleft,
+    index - 1,
+    postorder,
+    postleft,
+    postleft + leftSize - 1
+  );
+  node.right = buildFromInPost(
+    inorder,
+    index + 1,
+    inright,
+    postorder,
+    postleft + leftSize,
+    postright - 1
+  );
+
+  return node;
+}
+
 const tests = [
+  {
+    preorder: [1, 2, 3],
+    inorder: [1, 3, 2],
+    postorder: [3, 2, 1],
+  },
   {
     preorder: [3, 9, 20, 15, 7],
     inorder: [9, 3, 15, 20, 7],
@@ -98,5 +167,20 @@ for (let i = 0; i < tests.length; i++) {
     console.log(`${i}: passed`);
   } else {
     console.log(`${i}: expect ${test.postorder}, got ${postorder}`);
+  }
+}
+
+for (let i = 0; i < tests.length; i++) {
+  let test = tests[i];
+  let btree = buildFromInPostOrder(test.inorder, test.postorder);
+  let preorder = [];
+  visitPreorder(btree, (node) => {
+    preorder.push(node.val);
+  });
+
+  if (equals(preorder, test.preorder)) {
+    console.log(`${i}: passed`);
+  } else {
+    console.log(`${i}: expect ${test.preorder}, got ${preorder}`);
   }
 }
