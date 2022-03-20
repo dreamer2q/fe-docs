e9c16f85acf59e0e9c8ce416f5cb077928a50b62
Jack Li <dreamer22qq@gmail.com>
2022-03-20 12:20:40 +0800

    update

--- leet-code/btree/tree-node.js
+++ leet-code/btree/tree-node.js
@@ -7,6 +7,41 @@ export class TreeNode {
 }
 
 /**
+ * 前序遍历
+ * @param {TreeNode} tree
+ * @param {function(TreeNode)} cb
+ * @returns {void}
+ */
+export function visitPreorder(tree, cb) {
+  if (tree == null) {
+    return;
+  }
+
+  if (typeof cb === "function") {
+    cb(tree);
+  }
+  visitPreorder(tree.left, cb);
+  visitPreorder(tree.right, cb);
+}
+
+/**
+ * 中序遍历
+ * @param {TreeNode} tree
+ * @param {function(TreeNode)} cb
+ * @returns {void}
+ */
+export function visitInorder(tree, cb) {
+  if (tree == null) {
+    return;
+  }
+  visitInorder(tree.left, cb);
+  if (typeof cb === "function") {
+    cb(tree);
+  }
+  visitInorder(tree.right, cb);
+}
+
+/**
  * 后序遍历
  * @param {TreeNode} tree
  * @param {function(TreeNode)} cb
@@ -16,10 +51,8 @@ export function visitPostorder(tree, cb) {
   if (tree == null) {
     return;
   }
-
   visitPostorder(tree.left, cb);
   visitPostorder(tree.right, cb);
-
   if (typeof cb === "function") {
     cb(tree);
   }
